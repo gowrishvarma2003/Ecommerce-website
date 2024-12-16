31d67d9eacd0eff6466ba175a137173b223e4f60
gowrishvarma2003 <gowrishvarma@gmail.com>
2024-12-16 17:43:29 +0530

    milestone-8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,  SignupPage, ActivationPage} from "./Routes.js";
+import {LoginPage,  SignupPage,Home} from "./Routes.js";
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<LoginPage/>}/>
       <Route path="/sign-up" element={<SignupPage />} />
-      <Route path="/activation/:activation_token" element={<ActivationPage />} />
     </Routes>
     </BrowserRouter>
     
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,6 @@
 
 import LoginPage from "../src/pages/Login.jsx";
 import SignupPage from "../src/pages/SignupPage.jsx";
-import ActivationPage from "../src/pages/ActivationPage";
-export { SignupPage, LoginPage, ActivationPage};
+import Home from "./pages/Home.jsx";
+export { SignupPage, LoginPage, Home};
 
