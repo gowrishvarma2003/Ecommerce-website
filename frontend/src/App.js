c370d5c5afcd6d9c1c089a216120e8784d405cb1
gowrishvarma2003 <gowrishvarma@gmail.com>
2024-12-28 14:13:52 +0530

    milestone-13

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,18 @@ import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path="/sign-up" element={<SignupPage />} />
-      <Route path="/create-product" element={<CreateProduct />} />
-        <Route path="/myproducts" element={<MyProducts/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        {/* For new product */}
+        <Route path="/create-product" element={<CreateProduct />} />
+        {/* For edit product by id */}
+        <Route path="/create-product/:id" element={<CreateProduct />} />
+        {/* For viewing the user’s products */}
+        <Route path="/my-products" element={<MyProducts />} />
+      </Routes>
     </BrowserRouter>
-    
-  )
-}
-
+  );
+};
 export default App
